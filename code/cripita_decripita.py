efe1f51c2b14cc9c05f58b7cb8194c70e8dc4525
Lucas Felix <lucasfelixaguiar@gmail.com>
2023-10-27 15:42:55 -0300

    cria arquivo de constantes de texto e utilização dessas variáveis

--- code/cripita_decripita.py
+++ code/cripita_decripita.py
@@ -1,11 +1,13 @@
 # %%
+
+# Importa constantes de texto a partir de arquivo auxiliar
+from constantes_texto import *
+
 # Função que criptografar a mensagem.
-
-
 def codificar(mensagem: str, chave: str):
     mensagem_codificada = ""
 
-    #Garante que a chave e a mensagem tenham o mesmo tamanho.
+    # Garante que a chave e a mensagem tenham o mesmo tamanho.
     if len(chave) < len(mensagem):
         while len(chave) < len(mensagem):
             chave += chave
@@ -20,8 +22,6 @@ def codificar(mensagem: str, chave: str):
 
 
 # Função que descriptografa a mensagem.
-
-
 def decodificar(mensagem: str, chave: str):
     mensagem_decodificada = ""
 
@@ -40,8 +40,6 @@ def decodificar(mensagem: str, chave: str):
 
 
 # Função que transforma a chave.
-
-
 def transforma_chave(chave: str):
     chave = str(chave)
     chave_numerica = 0
@@ -53,76 +51,51 @@ def transforma_chave(chave: str):
 
 
 # Define o intervalo válido da funação chr().
-
-
 def ascii_extensao():
     invervalo = 1114112
     return invervalo
 
 
 # Define o intervalo do alfabeto português-BR.
-
-
 def alfabeto_extensao():
     extensao = 26
     return extensao
 
+def montar_cabecalho():
+    cabecalho = TEXTO_CABECALHO.format(
+        TEXTO_BARRA, 
+        "*"+TEXTO_NOME_PROGRAMA.center(len(TEXTO_BARRA)-2)+"*", 
+        TEXTO_BARRA
+    )
+    return cabecalho
 
 # Função principal que interage com o usuário.
-
-
 def main():
-
-# lista de variaveis contendo as mensagens que serão exibidas para o usuário
-
-    texto_entrada_opcao = "\nDigite um dos números abaixo para realizar a opção desejada:"
-    texto_opcoes = """
-[1] para codificar mensagem
-[2] para decodificar mensagem
-[3] para sair do programa
-"""
-    texto_pedido_codificada = "\nDigite a mensagem que será codificada: "
-    texto_pedido_decodificada = "\nDigite a mensagem que será decodificada: "
-
-    texto_entrada_invalida = "\nVocê inseriu uma opção inválida! Por favor, tente novamente. "
-
-    texto_pedido_chave_codificar = "\nDigite a chave que será usada para codificar a mensagem. "
-    texto_pedido_chave_decodificar = "\nDigite a chave que será usada para decodificar a mensagem. "
-
-    texto_lembrete_codificacao = "\nLembre-se que essa mesma chave é necessária para decodificar a mensagem. "
-    texto_lembrete_decodificacao = "\nLembre-se que a chave deve ser a mesma que foi utilizada na codificação. "
-
-    texto_sucesso_codificacao = "\nMensagem codificada com sucesso: "
-    texto_sucesso_decodificacao = "\nMensagem decodificada com sucesso: "
-
-    texto_encerrado = "\nPrograma encerrado!"
-
-
-    while True:
-        modo = input(f"{texto_entrada_opcao} \n{texto_opcoes}").strip()
+    print(montar_cabecalho())
+    continua_programa = True
+    while continua_programa:
+        modo = input(f"{TEXTO_ENTRADA_OPCOES} {TEXTO_OPCOES}").strip()
 
         if modo not in ['1', '2', '3']:
-            print(texto_entrada_invalida)
-            continue
+            print(TEXTO_ENTRADA_INVALIDA)
+        else:
+            if modo == '3':
+                print(TEXTO_ENCERRADO)
+                continua_programa = False
 
-        if modo == '3':
-            print(texto_encerrado)
-            break
+            elif modo == '1':
+                mensagem_entrada = input(TEXTO_PEDIDO_MENSAGEM.format("codificada"))
+                chave = input(TEXTO_PEDIDO_CHAVE.format("codificar") + TEXTO_LEMBRETE_CODIFICACAO)
 
-        if modo == '1':
-            entrada_mensagem_codificar = input(texto_pedido_codificada)
-            entrada_chave_codificar = input(f"{texto_pedido_chave_codificar} {texto_lembrete_codificacao}")
+                mensagem_codificada = codificar(mensagem=mensagem_entrada, chave=chave)
+                print(TEXTO_SUCESSO_PROCESSAMENTO.format("codificada") + mensagem_codificada)
 
-            print(texto_sucesso_codificacao)
-            print(codificar(mensagem=entrada_mensagem_codificar, chave=entrada_chave_codificar))
+            elif modo == '2':
+                entrada_mensagem_decodificar = input(TEXTO_PEDIDO_MENSAGEM.format("decodificada"))
+                entrada_chave_decodificar = input(TEXTO_PEDIDO_CHAVE.format("decodificar") + TEXTO_LEMBRETE_DECODIFICACAO)
 
-        elif modo == '2':
-            entrada_mensagem_decodificar = input(texto_pedido_decodificada)
-            entrada_chave_decodificar = input(f"{texto_pedido_chave_decodificar} {texto_lembrete_decodificacao}")
-
-            print(texto_sucesso_decodificacao)
-            print(decodificar(mensagem=entrada_mensagem_decodificar, chave=entrada_chave_decodificar))
-
+                mensagem_decodificada = decodificar(mensagem=entrada_mensagem_decodificar, chave=entrada_chave_decodificar)
+                print(TEXTO_SUCESSO_PROCESSAMENTO.format("decodificada") + mensagem_decodificada)
 
 if __name__ == "__main__":
     main()
